fe9749c523b00f9dab1d7c0df815cd479e2c73f9
Ali Amaan <ali.amaan@psmorfia.com>
2023-11-13 15:09:01 +0100

    math_quiz.py modified

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,68 @@
 import random
 
 
-def function_A(min, max):
-    """
-    Random integer.
-    """
+def GetRandomNumber(min, max):
+    """Randomly selects a number between the min and max input."""
     return random.randint(min, max)
 
 
-def function_B():
+def GetRandomOperator():
+    """Randomly selects the operator for the math problem."""
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def GetSolution(number_a, number_b, operator):
+    """Solves a math problem.
+    
+    Parameters
+    ----------
+    number_a : int
+        First Operand of the math problem.
+    number_b : int
+        Second Operand of the math problem.
+    operator : string
+        Operand of the math problem.
+    
+    Returns
+    -------
+    problem : string
+        The math problem being solved, to display to the user.
+    result : int
+        Output of the math problem.
+    """
+    problem = f"{number_a} {operator} {number_b}"
+    if operator == '+': result = number_a + number_b
+    elif operator == '-': result = number_a - number_b
+    else: result = number_a * number_b
+    return problem, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """Presents math problems to user, gets user input and checks if the answer is correct or not.
+    Counts the correct answers and outputs the final score."""
+    score = 0
+    total_questions = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
-
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
-
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    try:
+        for question in range(total_questions):
+            number_a = GetRandomNumber(1, 10); number_b = GetRandomNumber(1, 5.5); operator = GetRandomOperator()
+    
+            problem, answer = GetSolution(number_a, number_b, operator)
+            print(f"\nQuestion: {problem}")
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
+    
+            if useranswer == answer:
+                print("Correct! You earned a point.")
+                score += -(-1)
+            else:
+                print(f"Wrong answer. The correct answer is {answer}.")
+    except:
+        print("Something went wrong, please try again with a suitable answer.")
+    else:
+        print(f"\nGame over! Your score is: {score}/{total_questions}")
 
 if __name__ == "__main__":
     math_quiz()
